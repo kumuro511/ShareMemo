4f42699ad17bb3aff6192ba11f5d1e6e8b192189
Dai Yamamoto <gos415345@gmail.com>
2013-07-08 20:08:26 +0900

    First Commit

---
+++ ShareMemo/gen/jp/ac/titech/itpro/sdl/yamamoto/sharememo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jp.ac.titech.itpro.sdl.yamamoto.sharememo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
